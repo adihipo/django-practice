2ee30bc7fb1d72775aa52832625432cc72388996
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-01-07 15:42:40 +0100

    redirect to count

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'hi': 'by ÁDÁM GYARMATI'})
+
+
+def count(request):
+    return render(request, 'count.html')
