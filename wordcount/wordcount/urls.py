dca62c93714101a1a7105c6543cc454d7b9e4d56
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-01-07 16:10:26 +0100

    add redirect to home

--- wordcount/wordcount/urls.py
+++ wordcount/wordcount/urls.py
@@ -17,6 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('count/', views.count, name='count')
 ]
