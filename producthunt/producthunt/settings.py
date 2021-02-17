840896d3dbc6557da3190f79316d161d4c395334
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-02-17 12:04:33 +0100

    apps add to settings

--- producthunt/producthunt/settings.py
+++ producthunt/producthunt/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products.app.ProductsConfig',
+    'accounts.app.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
