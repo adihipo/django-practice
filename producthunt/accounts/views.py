9f054d5ef1c2dfeb640adc425fcab4da1d11eb41
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-02-17 13:06:23 +0100

    init signup backend

--- producthunt/accounts/views.py
+++ producthunt/accounts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password_conf']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                auth.login(request, user)
+                return redirect('home')
+    else:
+        return render(request, 'accounts/signup.html')
 
 
 def login(request):
